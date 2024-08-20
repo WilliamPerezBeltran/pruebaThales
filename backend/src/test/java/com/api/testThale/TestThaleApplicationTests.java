75a9327e45fcefd7e73e50d3d2acccc90a1faa7b
WilliamPerezBeltran <williampbeltranprogramador@gmail.com>
2024-08-20 18:26:29 -0500

    test request

---
+++ backend/src/test/java/com/api/testThale/TestThaleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.testThale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestThaleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
