42a27311ca0b4adb7387c6beef0293c8ec6a6f21
WilliamPerezBeltran <williampbeltranprogramador@gmail.com>
2024-08-20 20:03:37 -0500

    Front-end: set up skeleton

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Button from '@mui/material/Button';
+import './App.scss';
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="app">
+      <header className="app-header">
+        <h1>Welcome to My Material Design App</h1>
+        <Button variant="contained" color="primary">
+          Material UI Button
+        </Button>
       </header>
     </div>
   );
-}
+};
 
 export default App;
