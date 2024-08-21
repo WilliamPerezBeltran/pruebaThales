5c9e8f207b1762d1d5a6063f842f6fbb8ce54fce
WilliamPerezBeltran <williampbeltranprogramador@gmail.com>
2024-08-21 06:34:06 -0500

    Back-end: Set up CORS

--- backend/src/main/java/com/api/testThale/config/AppConfig.java
+++ backend/src/main/java/com/api/testThale/config/AppConfig.java
@@ -12,3 +12,27 @@ public class AppConfig {
         return new RestTemplate();
     }
 }
+
+// package com.api.testThale.config;
+
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.web.servlet.config.annotation.CorsRegistry;
+// import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+// import org.springframework.web.client.RestTemplate;
+
+// @Configuration
+// public class AppConfig implements WebMvcConfigurer {
+
+//     @Override
+//     public void addCorsMappings(CorsRegistry registry) {
+//         registry.addMapping("/**")
+//                 .allowedOrigins("http://localhost:3000")
+//                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+//                 .allowedHeaders("*");
+//     }
+//  	@Bean
+//     public RestTemplate restTemplate() {
+//         return new RestTemplate();
+//     }
+// }
